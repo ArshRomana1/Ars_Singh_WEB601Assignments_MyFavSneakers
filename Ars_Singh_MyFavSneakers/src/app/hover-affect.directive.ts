915e3e7ee6dcb29f1cfcf8192dc7886f9980244d
ArshRomana1 <113393599+ArshRomana1@users.noreply.github.com>
2023-06-13 21:40:27 -0400

    added hover and bold directive

--- Ars_Singh_MyFavSneakers/src/app/hover-affect.directive.ts
+++ Ars_Singh_MyFavSneakers/src/app/hover-affect.directive.ts
@@ -1,10 +1,37 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appHoverAffect]'
 })
 export class HoverAffectDirective {
+  @Input() affectStyle?: string;
+  private originalBorder?: string;
 
-  constructor() { }
+  constructor(private elementRef: ElementRef, private renderer: Renderer2) { }
+
+  @HostListener('mouseenter') onMouseEnter() {
+    this.addStyles();
+  }
+
+  @HostListener('mouseleave') onMouseLeave() {
+    this.removeStyles();
+  }
+
+  private addStyles() {
+    if (this.affectStyle === 'underline') {
+      this.renderer.setStyle(this.elementRef.nativeElement, 'text-decoration' && 'color', 'underline' && 'green');
+    } else if (this.affectStyle === 'bold') {
+      this.renderer.setStyle(this.elementRef.nativeElement, 'font-weight', 'bold');
+    }
+  }
+
+  private removeStyles() {
+    if (this.affectStyle === 'underline') {
+      this.renderer.removeStyle(this.elementRef.nativeElement, 'text-decoration' && 'color');
+    } else if (this.affectStyle === 'bold') {
+      this.renderer.removeStyle(this.elementRef.nativeElement, 'font-weight');
+
+    }
+  }
 
 }
