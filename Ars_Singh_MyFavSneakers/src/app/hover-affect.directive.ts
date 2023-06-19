297a634aae02520c3b2f9f5a88ac2efba13190b3
ArshRomana1 <113393599+ArshRomana1@users.noreply.github.com>
2023-06-19 16:16:02 -0400

    Changed the function
    updated the function for styling in ts and html file

--- Ars_Singh_MyFavSneakers/src/app/hover-affect.directive.ts
+++ Ars_Singh_MyFavSneakers/src/app/hover-affect.directive.ts
@@ -1,13 +1,12 @@
-import { Directive, ElementRef, HostListener, Input, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input} from '@angular/core';
 
 @Directive({
   selector: '[appHoverAffect]'
 })
 export class HoverAffectDirective {
   @Input() affectStyle?: string;
-  private originalBorder?: string;
 
-  constructor(private elementRef: ElementRef, private renderer: Renderer2) { }
+  constructor(private elementRef: ElementRef) { }
 
   @HostListener('mouseenter') onMouseEnter() {
     this.addStyles();
@@ -18,19 +17,25 @@ export class HoverAffectDirective {
   }
 
   private addStyles() {
-    if (this.affectStyle === 'underline') {
-      this.renderer.setStyle(this.elementRef.nativeElement, 'text-decoration' && 'color', 'underline' && 'green');
+    if (this.affectStyle === 'color') {
+      this.elementRef.nativeElement.style.color = 'green';
     } else if (this.affectStyle === 'bold') {
-      this.renderer.setStyle(this.elementRef.nativeElement, 'font-weight', 'bold');
+      this.elementRef.nativeElement.style.fontWeight='bold';
+    }
+    else if (this.affectStyle === 'border') {
+      this.elementRef.nativeElement.style.border='2px solid black';
     }
   }
 
   private removeStyles() {
-    if (this.affectStyle === 'underline') {
-      this.renderer.removeStyle(this.elementRef.nativeElement, 'text-decoration' && 'color');
+    if (this.affectStyle === 'color') {
+      this.elementRef.nativeElement.style.color="";
     } else if (this.affectStyle === 'bold') {
-      this.renderer.removeStyle(this.elementRef.nativeElement, 'font-weight');
+      this.elementRef.nativeElement.style.fontWeight="";
 
+    }
+    else if (this.affectStyle === 'border') {
+      this.elementRef.nativeElement.style.border='';
     }
   }
 
